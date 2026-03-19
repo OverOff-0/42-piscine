1da7e98fd227c668d1624a47810f39e2e95bc2e2
mcheddad mcheddad <mcheddad@f2r6s6.paris.42.school>
2026-03-19 14:31:10 +0100

    0

--- training/ex_putnbr/buzzfizz.c
+++ training/ex_putnbr/buzzfizz.c
@@ -6,23 +6,45 @@
 /*   By: mcheddad <mcheddad@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 13:40:31 by mcheddad          #+#    #+#             */
-/*   Updated: 2026/03/19 13:45:08 by mcheddad         ###   ########.fr       */
+/*   Updated: 2026/03/19 14:30:35 by mcheddad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+		ft_putnbr((nb % 10) + '0');
+}
+
 void	fizzbuzz()
 {
 	int	count;
 
-	if (count % 15)
-		write(1, "fizzbuzz", 8);
-	else if (count % 3)
-		write(1, "fizz", 4);
-	else if (count % 5)
-		write(1, "buzz", 4);
-	else
-		count++;
-	return (count);
+	count = 1;
+	while (count <= 100)
+	{
+		if (count % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (count % 3 == 0)
+			write(1, "fizz", 4);
+		else if (count % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(count);
+			ft_putchar('\n');
+			count++;
+	}
+}
+
+int	main(void)
+{
+	fizzbuzz();
 }
