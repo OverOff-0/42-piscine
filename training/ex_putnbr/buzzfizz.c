a88a70dd42aa76ecb9ac0151096c3334b8dfdd74
mcheddad mcheddad <mcheddad@f0r12s17.paris.42.school>
2026-03-20 11:26:14 +0100

    0

---
+++ training/ex_putnbr/buzzfizz.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   buzzfizz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcheddad <mcheddad@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 13:40:31 by mcheddad          #+#    #+#             */
+/*   Updated: 2026/03/19 14:35:08 by mcheddad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');
+}
+
+void	fizzbuzz()
+{
+	int	count;
+
+	count = 1;
+	while (count <= 100)
+	{
+		if (count % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (count % 3 == 0)
+			write(1, "fizz", 4);
+		else if (count % 5 == 0)
+			write(1, "buzz", 4);
+		else
+		{
+			ft_putnbr(count);
+		}
+		ft_putchar('\n');
+		count++;
+	}
+}
+
+int	main(void)
+{
+	fizzbuzz();
+}
