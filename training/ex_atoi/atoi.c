d8a631b5aa3b3bf25dde32917f66d8b2ea1aff0c
mcheddad mcheddad <mcheddad@f0r12s17.paris.42.school>
2026-03-20 11:21:30 +0100

    0

---
+++ training/ex_atoi/atoi.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atoi.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcheddad <mcheddad@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/20 11:02:26 by mcheddad          #+#    #+#             */
+/*   Updated: 2026/03/20 11:21:18 by mcheddad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
+}
+
+/*void	ft_purchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 10)
+		putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		ft_putnbr(ft_atoi(argv[1]));
+	}
+	write(1, "\n", 1);
+	return (0);
+}*/
